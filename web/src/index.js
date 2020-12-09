9ed7d7c727e43e42f0766f519ef704dafac8a843
cratobi <ci.riyadh@gmail.com>
2020-12-09 07:03:03 +0600

    📐Setups

--- web/src/index.js
+++ web/src/index.js
@@ -1,10 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './container/App';
+import React, { StrictMode } from 'react'
+import ReactDOM from 'react-dom'
+import reportWebVitals from './reportWebVitals'
+import { BrowserRouter as Router } from 'react-router-dom'
+
+import configureStore from './store'
+import { Provider } from 'react-redux'
+
+import './style/utilities.scss'
+import './style/global.scss'
+
+import App from './container/App'
+
+const store = configureStore()
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <StrictMode>
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>,
+  </StrictMode>,
   document.getElementById('root')
-);
+)
+
+reportWebVitals(console.log);
